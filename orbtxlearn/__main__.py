cc122c32cfdafd2067d376770fd11d20d50b7186
Tyler Hanks <teh.hanks@gmail.com>
2018-11-21 00:15:58 -0500

    Changed a 2 to a 5

--- orbtxlearn/__main__.py
+++ orbtxlearn/__main__.py
@@ -8,7 +8,7 @@ from . import Spy
 
 def spy_update_callback(event_type: str, spy: Spy) -> None:
     if event_type == 'state' and not spy.playing:
-        time.sleep(2)
+        time.sleep(5)
         spy.round_reset()
 
     print(f'gameon: {spy.playing}, score: {spy.score}, dir: {spy.direction}, pps: {spy.pps:.2f}')
@@ -16,10 +16,11 @@ def spy_update_callback(event_type: str, spy: Spy) -> None:
 def main(host: str = 'localhost', port: int = 2600) -> None:
     spy = Spy(host, port, spy_update_callback)
     while True:
-        time.sleep(random.random())
-        pyautogui.keyDown('space')
-        time.sleep(random.random())
-        pyautogui.keyUp('space')
+        if spy.playing:
+            time.sleep(random.random())
+            pyautogui.keyDown('space')
+            time.sleep(random.random())
+            pyautogui.keyUp('space')
 
 if __name__ == '__main__':
     if len(sys.argv) == 3:
