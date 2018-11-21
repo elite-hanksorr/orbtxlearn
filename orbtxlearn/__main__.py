dbe351526b223a349b577a2956a3324f056a9a05
Kevin Orr <kevinorr54@gmail.com>
2018-11-21 00:04:13 -0500

    AI v0.1: randomly spam the spacebar

--- orbtxlearn/__main__.py
+++ orbtxlearn/__main__.py
@@ -1,18 +1,25 @@
+import random
 import sys
 import time
+
+import pyautogui
 
 from . import Spy
 
 def spy_update_callback(event_type: str, spy: Spy) -> None:
     if event_type == 'state' and not spy.playing:
-        time.sleep(1.5)
+        time.sleep(2)
         spy.round_reset()
 
     print(f'gameon: {spy.playing}, score: {spy.score}, dir: {spy.direction}, pps: {spy.pps:.2f}')
 
 def main(host: str = 'localhost', port: int = 2600) -> None:
     spy = Spy(host, port, spy_update_callback)
-    time.sleep(100000)
+    while True:
+        time.sleep(random.random())
+        pyautogui.keyDown('space')
+        time.sleep(random.random())
+        pyautogui.keyUp('space')
 
 if __name__ == '__main__':
     if len(sys.argv) == 3:
