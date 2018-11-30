bbec41b9b19e5068ab19ffb07f8fb3bafa44e954
Kevin Orr <kevinorr54@gmail.com>
2018-11-30 11:22:15 -0500

    Tweak hyperparameters

--- orbtxlearn/config.py
+++ orbtxlearn/config.py
@@ -14,11 +14,11 @@ episode_strftime = '%Y-%m-%d_%H-%M-%S'
 class params:
     image_size = 480  # Width and height of screenshots
 
-    explore_rate = 0.40  # Probability of ignoring model and choosing a random action
+    explore_rate = 0.10  # Probability of ignoring model and choosing a random action
 
-    reward_nothing = -0.1  # Passive reward for accomplishing nothing but still not failing
+    reward_nothing = -0.4  # Passive reward for accomplishing nothing but still not failing
     reward_score = 1  # Reward for scoring one OrbtXL point
-    reward_death = -2  # Reward (penalty) for dying
+    reward_death = -0.8  # Reward (penalty) for dying
     reward_discount_10db = 3.0  # Seconds to reach -10dB (10%) discount
 
     # [(filter_size, stride_size, output_depth)]
@@ -32,7 +32,7 @@ class params:
     post_lstm_fc_nodes = []
 
 class training:
-    learning_rate = 0.00005
+    learning_rate = 0.0002
     minimum_random_episodes = 5  # Minimum episodes to collect with random weights before we can train
     batch_size = 1
     max_checkpoints = 10
